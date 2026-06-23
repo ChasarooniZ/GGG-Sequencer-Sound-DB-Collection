d17873e488c9a60f62f74451385c8ae01ff863b0
cadowtin <chasdowtin@gmail.com>
2026-06-23 14:45:27 -0500

    DB of stuff

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,20 @@
-Hooks.once('init', async function() {
+import { database as soundDb, DB_PREFIX as soundDbPrefix } from "./soundD.jsb";
+//Code lovingly borrowed from PF2e Grphics
+Hooks.once("init", async function () {
+  Hooks.once("sequencerReady", () => {
+    Sequencer.Database.registerEntries(soundDbPrefix, soundDb);
+    Sequencer.Database.registerEntries(assetDbPrefix, videoDb);
+  });
 
+  if (import.meta.hot) {
+    // Prevents reloads
+    import.meta.hot.accept();
+
+    if (window.Sequencer) {
+      Sequencer.Database.registerEntries(soundDbPrefix, soundDb, false, true);
+      Sequencer.Database.registerEntries(assetDbPrefix, videoDb, false, true);
+    }
+  }
 });
 
-Hooks.once('ready', async function() {
-
-});
+Hooks.once("ready", async function () {});
---
+++ scripts/soundDB.js
@@ -0,0 +1,30 @@
+export const DB_PREFIX = "graphics-sfx";
+
+// Start from 01
+const p = "modules/pf2e-graphics/assets/library/sounds";
+export const database = {
+  strikes: {
+    generic: {},
+    weapons: { melee: {}, ranged: {} },
+    natural: {},
+  },
+  magic: {
+    arcane: {},
+    primal: {},
+    acid: {},
+    air: {},
+    cold: {},
+    fire: {},
+    mental: {},
+    sonic: {},
+    vitality: {},
+    void: {},
+    water: {},
+  },
+  equipment: {},
+  tasks: {
+    crafting: {},
+    cooking: {},
+  },
+  misc: {},
+};
